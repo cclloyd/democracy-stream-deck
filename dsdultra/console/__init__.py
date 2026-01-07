473bf6c9220d58041db520b343530324b3a0cb6f
cclloyd <cclloyd@cclloyd.com>
2026-01-07 01:02:42 -0500

    WIP: updated windows console

--- dsdultra/console/__init__.py
+++ dsdultra/console/__init__.py
@@ -1,9 +1,9 @@
 from dsdultra.util import is_windows
 
 
-def show_console():
+def show_console(dsd=None):
     if is_windows():
        from .win import show_console as con
     else:
         from .linux import show_console as con
-    con()
+    con(dsd=dsd)
--- dsdultra/console/win.py
+++ dsdultra/console/win.py
@@ -1,5 +1,10 @@
-from dsdultra.util import is_frozen
+from dsdultra.args import parse_args
+from dsdultra.util import is_frozen, is_windows
 
 
-def show_console(icon=None, item=None):
-    pass
+def show_console(dsd=None, *args, **kwargs):
+    if not is_frozen():
+        return
+    if not is_windows():
+        return
+    args = parse_args()
