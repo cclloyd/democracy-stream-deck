f609364979da20f44f305e231181b674a592500d
cclloyd <cclloyd@cclloyd.com>
2026-06-05 15:45:13 -0400

    Cleaned up build command

--- dsdultra/build/linux.py
+++ dsdultra/build/linux.py
@@ -6,15 +6,15 @@ from nuitka.__main__ import main as nuitka_main
 def build_executable():
     sys.argv = [
         'nuitka',  # dummy program name
-        '--include-data-dir=dsdultra/assets=dsdultra/assets',
         '--standalone',
         '--assume-yes-for-downloads',
-        f'--output-filename=dsd',
+        '--output-filename=dsd',
+        '--no-deployment-flag=self-execution',
+        '--nofollow-import-to=dsdultra.build',
+        '--include-data-dir=dsdultra/assets=dsdultra/assets',
         '--enable-plugin=pyqt6',
         '--include-package=StreamDeck',
         '--include-package=Xlib',
-        '--no-deployment-flag=self-execution',
-        '--nofollow-import-to=dsdultra.build',
         '--output-dir=build',
         'dsdultra/',
     ]
