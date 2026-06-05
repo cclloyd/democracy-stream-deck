49a19e2f63c68fea3e99b8969c15563c96e349fa
cclloyd <cclloyd@cclloyd.com>
2026-06-05 15:40:33 -0400

    Removed windows installer wizard in favor of bundled dll

--- dsdultra/installer/__init__.py
+++ dsdultra/installer/__init__.py
@@ -1,23 +1,25 @@
-from dsdultra.util import is_windows
+from dsdultra.util import is_linux
 
 
 class InstallerWizard:
+    wizard = None
+
     def __init__(self):
-        if is_windows():
-            from .win import WindowsInstallerWizard
-            self.wizard = WindowsInstallerWizard()
-        else:
+        if is_linux():
             from .linux import LinuxInstallerWizard
             self.wizard = LinuxInstallerWizard()
 
     def silent_install(self):
-        self.wizard.silent_install()
+        if self.wizard:
+            self.wizard.silent_install()
 
     def prompt_library_install(self):
-        self.wizard.prompt_library_install()
+        if self.wizard is not None:
+            self.wizard.prompt_library_install()
 
     def create_shortcut(self):
-        self.wizard.create_shortcut()
+        if self.wizard:
+            self.wizard.create_shortcut()
 
     def __getattr__(self, name):
         return getattr(self.wizard, name)
